38b75958152f880dba0cb81cd3736b848940a4f0
syfdn31 <ahmadsyaifudin2005@gmail.com>
2026-04-07 13:48:33 +0700

    Refactor scroll animation and navbar active link logic

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// SMOOTH SCROLL SNAP (biar per section)
-document.querySelectorAll("a[href^='#']").forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        const target = document.querySelector(this.getAttribute("href"));
-        target.scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
-
-
-// ANIMASI MUNCUL SAAT SCROLL
+// ANIMASI SCROLL
 const sections = document.querySelectorAll(".section");
 
 const observer = new IntersectionObserver((entries) => {
@@ -28,16 +15,15 @@ sections.forEach(section => {
     observer.observe(section);
 });
 
-
-// NAVBAR ACTIVE LINK
+// NAV ACTIVE
 const navLinks = document.querySelectorAll("nav a");
 
 window.addEventListener("scroll", () => {
     let current = "";
 
     sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        if (scrollY >= sectionTop - 100) {
+        const top = section.offsetTop;
+        if (scrollY >= top - 100) {
             current = section.getAttribute("id");
         }
     });
